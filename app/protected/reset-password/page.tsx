bc7e60ef1c62549bf099bd55b83977f9db3f19ad
Lucius Scala <luciustheperson@gmail.com>
2025-07-04 13:35:18 -0700

    finalizing build

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -2,7 +2,6 @@ import { resetPasswordAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 export default async function ResetPassword(props: {
   searchParams: Promise<Message>;
